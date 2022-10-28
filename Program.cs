cbe2f3b7ee03a0f99ec35ff8f6b6e1b396859667
Enver Ismailov <113299648+Enver074@users.noreply.github.com>
2022-10-28 16:02:07 +0300

    Вторая задача

--- Program.cs
+++ Program.cs
@@ -36,3 +36,33 @@
 //     }
 //     return result;
 // }
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3
+
+
+// int[,] array = GetArray(3,4,0,10);
+
+// PrintArray(array);
+// Console.WriteLine();
+
+// average(array);
+
+// double[] average(int[,] array){
+//     Console.Write($"Среднее арифметическое каждого столбца: ");
+//     double[] sum=new double [4];
+//     for (int i=0; i < array.GetLength(1); i++)
+//     {
+//         for(int j=0; j < array.GetLength(0); j++)
+//         {
+//             sum[i] += array[j, i];
+//         }
+//         Console.Write(Math.Round(sum[i] / array.GetLength(0), 2) + " ");
+//     }
+//     return sum;
+//     }
